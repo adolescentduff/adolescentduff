97d9305a58f184419e02f69835456afddef5b286
adolescentduff <87834676+adolescentduff@users.noreply.github.com>
2021-07-22 18:02:49 -0400

    Add files via upload

---
+++ Banking Application/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Account conlin = new Account("Conlin Duffy", 195056);
+
+        conlin.showMenu();
+
+    }
+}
